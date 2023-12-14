77016aad45831f22a65383eaf17c666d37ec8f4e
MattSun <mattmsun@gmail.com>
2023-12-14 14:58:09 +1100

    display platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Game } from "../hooks/useGame";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+
 interface Props {
   game: Game;
 }
@@ -10,6 +12,12 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        {/* {game.platforms.map(({ platform }) => (
+          <Text>{platform.name}</Text>
+        ))} */}
+        <PlatformIconList
+          platforms={game.parent_platforms.map(({ platform }) => platform)}
+        />
       </CardBody>
     </Card>
   );
