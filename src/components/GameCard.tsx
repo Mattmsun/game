318c0b98d9865567334f6d7d1166ff99ee860a70
MattSun <mattmsun@gmail.com>
2023-12-15 12:43:58 +1100

    optimize the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,13 +13,13 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack marginBottom={2} justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map(({ platform }) => platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
