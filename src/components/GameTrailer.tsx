f7d0fd0a268c2470b7473e596dab875b0910e7b6
MattSun <mattmsun@gmail.com>
2024-01-11 22:58:40 +1100

    build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,22 @@
+import { Spinner } from "@chakra-ui/react";
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useTrailers(gameId);
+  if (isLoading) return <Spinner />;
+  if (error) throw error;
+
+  const firstTrailer = data?.results[0];
+  return firstTrailer ? (
+    <video
+      src={firstTrailer.data[480]}
+      poster={firstTrailer.preview}
+      controls
+    />
+  ) : null;
+};
+
+export default GameTrailer;
