919beeb2d40294d76a9d62017cf2aeeded014550
Mattmsun <71315329+Mattmsun@users.noreply.github.com>
2024-08-01 07:55:20 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  outputPath: 'build', // 确保这里的 outDir 设置为你想要的输出目录
+  output_location: 'build', // 确保这里的 outDir 设置为你想要的输出目录
 
 })
